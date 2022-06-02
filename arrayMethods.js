8751b7fc064d826b36f4be41b24fe36ecfff9d45
Zack Rogers <zack.rogers.t@gmail.com>
2022-06-02 15:20:06 -0600

    added solution to prob 1

--- arrayMethods.js
+++ arrayMethods.js
@@ -7,7 +7,7 @@
 ////////// PROBLEM 1 //////////
 
 // Do not edit the code below.
-const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
+const mixedNumbers = [6, 3, 1, 7, 5, 2, 6, 8, 9, 4, 2, 7, 9, 3, 1, 8, 4, 3];
 // Do not edit the code above.
 
 /*
@@ -18,9 +18,10 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
 */
 
 // CODE HERE
-const evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
 
 
+const evenNumbers = mixedNumbers.filter(elem => elem % 2 === 0);
+// console.log(evenNumbers);
 
 ////////// PROBLEM 2 //////////
 
@@ -39,7 +40,7 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 // CODE HERE
-const postTaxPrices // = prices.map(/* Provide Your Callback Here );
+// const postTaxPrices // = prices.map(/* Provide Your Callback Here );
 
 
 
@@ -57,22 +58,22 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 // CODE HERE
-const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+// const totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
 
 
 
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
-const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulbabunny","CP":135},
-{"monster":"Bulbabunny","CP":250},{"monster":"Ponylopse","CP":277},{"monster":"Ponylopse","CP":184},
-{"monster":"Pikadoughnet","CP":207},{"monster":"Bulbabunny","CP":139},{"monster":"Pikadoughnet","CP":47},
-{"monster":"Pikadoughnet","CP":175},{"monster":"WaterHorsia","CP":26},{"monster":"Ponylopse","CP":19},
-{"monster":"Pikadoughnet","CP":218},{"monster":"Charaflier","CP":101},{"monster":"WaterHorsia","CP":96},
-{"monster":"Pikadoughnet","CP":253},{"monster":"Sandmush","CP":146},{"monster":"Bulbabunny","CP":247},
-{"monster":"Charaflier","CP":55},{"monster":"Bulbabunny","CP":72},{"monster":"Pikadoughnet","CP":300},
-{"monster":"Sandmush","CP":262},{"monster":"Sandmush","CP":25},{"monster":"Charaflier","CP":215},
-{"monster":"Ponylopse","CP":125},{"monster":"Bulbabunny","CP":178}];
+const monstersInYourPocket = [{ "monster": "Bulbabunny", "CP": 156 }, { "monster": "Bulbabunny", "CP": 135 },
+{ "monster": "Bulbabunny", "CP": 250 }, { "monster": "Ponylopse", "CP": 277 }, { "monster": "Ponylopse", "CP": 184 },
+{ "monster": "Pikadoughnet", "CP": 207 }, { "monster": "Bulbabunny", "CP": 139 }, { "monster": "Pikadoughnet", "CP": 47 },
+{ "monster": "Pikadoughnet", "CP": 175 }, { "monster": "WaterHorsia", "CP": 26 }, { "monster": "Ponylopse", "CP": 19 },
+{ "monster": "Pikadoughnet", "CP": 218 }, { "monster": "Charaflier", "CP": 101 }, { "monster": "WaterHorsia", "CP": 96 },
+{ "monster": "Pikadoughnet", "CP": 253 }, { "monster": "Sandmush", "CP": 146 }, { "monster": "Bulbabunny", "CP": 247 },
+{ "monster": "Charaflier", "CP": 55 }, { "monster": "Bulbabunny", "CP": 72 }, { "monster": "Pikadoughnet", "CP": 300 },
+{ "monster": "Sandmush", "CP": 262 }, { "monster": "Sandmush", "CP": 25 }, { "monster": "Charaflier", "CP": 215 },
+{ "monster": "Ponylopse", "CP": 125 }, { "monster": "Bulbabunny", "CP": 178 }];
 // Do not edit the code above.
 
 /*
@@ -82,17 +83,17 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 // CODE HERE
-const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+// const myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
 
 
 
 ////////// PROBLEM 5 //////////
 
 // Do not edit code below.
-const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax":0.11},
-{"price":80,"tax":0.11},{"price":69,"tax":0.06},{"price":68,"tax":0.14},
-{"price":72,"tax":0.14},{"price":51,"tax":0.09},{"price":89,"tax":0.15},
-{"price":48,"tax":0.13}];
+const orders = [{ "price": 15, "tax": 0.09 }, { "price": 42, "tax": 0.07 }, { "price": 56, "tax": 0.11 },
+{ "price": 80, "tax": 0.11 }, { "price": 69, "tax": 0.06 }, { "price": 68, "tax": 0.14 },
+{ "price": 72, "tax": 0.14 }, { "price": 51, "tax": 0.09 }, { "price": 89, "tax": 0.15 },
+{ "price": 48, "tax": 0.13 }];
 // Do not edit code above.
 
 /*
@@ -106,13 +107,13 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
 ////////// PROBLEM 6 //////////
 
 // Do not edit the code below.
-const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
-{"owner":"Bob","price":73},{"owner":"Barry","price":57},{"owner":"Barry","price":128},
-{"owner":"Bob","price":119},{"owner":"Barry","price":133},{"owner":"Barry","price":27},
-{"owner":"Barry","price":138},{"owner":"Bob","price":68},{"owner":"Bob","price":50},
-{"owner":"Barry","price":9},{"owner":"Bob","price":123},{"owner":"Bob","price":135},
-{"owner":"Barry","price":30},{"owner":"Barry","price":129},{"owner":"Barry","price":38},
-{"owner":"Bob","price":133},{"owner":"Barry","price":109},{"owner":"Bob","price":115}];
+const purchases = [{ "owner": "Barry", "price": 103 }, { "owner": "Bob", "price": 75 },
+{ "owner": "Bob", "price": 73 }, { "owner": "Barry", "price": 57 }, { "owner": "Barry", "price": 128 },
+{ "owner": "Bob", "price": 119 }, { "owner": "Barry", "price": 133 }, { "owner": "Barry", "price": 27 },
+{ "owner": "Barry", "price": 138 }, { "owner": "Bob", "price": 68 }, { "owner": "Bob", "price": 50 },
+{ "owner": "Barry", "price": 9 }, { "owner": "Bob", "price": 123 }, { "owner": "Bob", "price": 135 },
+{ "owner": "Barry", "price": 30 }, { "owner": "Barry", "price": 129 }, { "owner": "Barry", "price": 38 },
+{ "owner": "Bob", "price": 133 }, { "owner": "Barry", "price": 109 }, { "owner": "Bob", "price": 115 }];
 // Do not edit the code above.
 
 /*
